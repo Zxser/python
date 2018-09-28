5836ebd7d1c520f886274866e7e35eab6a78f626
mengdaya <meng352247816@outlook.com>
2018-09-28 19:36:20 +0800

    add a new file

--- Pythontest/day7.py
+++ Pythontest/day7.py
@@ -8,3 +8,21 @@ print (friends['bob'])
 print (friends)
 for key in friends:
     print (key,":",friends[key])
+
+
+a1={'a':1,'b':2}
+a2={'b':2,'a':1}
+print(a1==a2)
+print (a1 !=a2)
+
+
+friends = {'tom': '111-222-333', 'bob': '888-999-666', 'jerry': ' 666-33-111'}
+print (friends.keys())
+print (friends.popitem())
+print (friends)
+
+friends = {'tom': '111-222-333', 'bob': '888-999-666', 'jerry': ' 666-33-111'}
+print (friends.items())
+print (friends.values())
+print (friends.get('tom'))
+print (friends.get('a','bucunzai'))
