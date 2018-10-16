22577ee5f8e9acbb1ca636cbfdc99416708db473
mengdaya <meng352247816@outlook.com>
2018-10-16 20:00:58 +0800

    Add a new file

--- Pythontest/day11.py
+++ Pythontest/day11.py
@@ -108,6 +108,35 @@ func()
 
 
 
+def bigger(a,b):
+    if a>b:
+        return a,b
+    else:
+        return b,a
+s=bigger(12,100)
+print (s,type(s))
+
+
+def print_max(x,y):
+    '''Prints the maximum of two numbers.
+    The two values must be integers.'''
+    x=int(x)
+    y=int(y)
+    if x>y:
+        print (x,'is maximum')
+    else:
+        print (y,'is maximum')
+print_max(3,5)
+print (print_max.__doc__)
+
+mylist=[1,2,3,4]
+for i in mylist:
+    print (i)
+
+
+
+
+
 
 
 
