9f3fecef02f276a13708d21f442b7213b8798b2d
mengdaya <meng352247816@outlook.com>
2019-01-03 20:58:56 +0800

    Add a new file

---
+++ Pythontest/day15.py
@@ -0,0 +1,25 @@
+names=['michael','tom','jerry']
+for name in names:
+    print (name)
+sum=0
+for x in range(5):
+    sum=sum+x
+    print (sum)
+
+# while循环
+sum=0
+n=99
+while n >0:
+    sum=sum+n
+    n=n-2
+print (sum)
+
+L=['bart','tom','jerry']
+for x in L:
+    print ('hello',x)
+
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print (d['Bob'])
+
+d['tom']=90
+print (d['tom'])
---
+++ Pythontest/面向对象的编程/day16.py
@@ -0,0 +1,15 @@
+# -*- coding:utf=8 -*-
+# a=['a','b','c']
+# a.sort()
+# print (a)
+
+
+
+s1=12.34
+r1=s1*3.14*2
+print (r1)
+
+s1=1
+s2=2
+print (max(s1,s2))
+print (max(2,3,1,-5))
